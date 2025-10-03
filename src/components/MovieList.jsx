b9719dd9f33db7da4672a03b1a24109f17d7da31
James Hughes <james.a.hughes@gmail.com>
2025-10-03 17:23:18 -0500

    click button to remove movie

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,24 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { removeMovie } from "../movieSlice";
 
 const MovieList = () => {
   const movies = useSelector((state) => state.movies.movies);
+  const dispatch = useDispatch();
+
+  const handleRemoveMovie = (id) => {
+    dispatch(removeMovie(id));
+  };
 
   return (
     <div>
       <h1>Movie List</h1>
       {movies.map((movie) => (
-        <div key={movie.id}>{movie.name}</div>
+        <div key={movie.id}>
+          {movie.name}
+          <button onClick={() => handleRemoveMovie(movie.id)}>
+            Delete Movie
+          </button>
+        </div>
       ))}
     </div>
   );
